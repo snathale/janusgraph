7bc4c1e869cb3c44871566a1eaa960d3a46061d7
snathale <silva.nathale@gmail.com>
2018-07-19 20:35:09 -0300

    create klotin project with maven

---
+++ access-control/src/main/kotlin/ntopus/kofre/com/Hello.kt
@@ -0,0 +1,6 @@
+package ntopus.kofre.com
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ access-control/src/test/kotlin/ntopus/kofre/com/HelloTest.kt
@@ -0,0 +1,8 @@
+package ntopus.kofre.com
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
